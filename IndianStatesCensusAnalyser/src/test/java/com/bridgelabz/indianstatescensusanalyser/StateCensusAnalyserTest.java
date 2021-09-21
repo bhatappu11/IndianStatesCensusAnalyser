501df406d87abb4367bb94193ef55b97b783599b
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-21 22:51:33 +0530

    ADDED: test case to check for incorrect file

--- IndianStatesCensusAnalyser/src/test/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
+++ IndianStatesCensusAnalyser/src/test/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyserTest.java
@@ -1,10 +1,12 @@
 package com.bridgelabz.indianstatescensusanalyser;
 
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.Assert;
 
 public class StateCensusAnalyserTest {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./main/resources/IndiaStateCensusData.csv";
 	
 	@Test
 	public void givenStateCensusCsvFile_ShouldReturn_CorrectNumberOfRecords() {
@@ -17,5 +19,16 @@ public class StateCensusAnalyserTest {
 			e.printStackTrace();
 		}
 		
+	}
+	@Test
+	public void givenStateCensusCsvFile_IfIncorrect_ReturnsACustomException() {
+		try {
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.loadIndianCensusData(WRONG_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+		}
 	}
 }
