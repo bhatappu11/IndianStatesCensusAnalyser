b72cff50fec37b5172d8d16fe8f7d948b6564c6b
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-21 23:41:29 +0530

    MODIFIED: added method to load state code csv file

--- IndianStatesCensusAnalyser/src/main/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyser.java
+++ IndianStatesCensusAnalyser/src/main/java/com/bridgelabz/indianstatescensusanalyser/StateCensusAnalyser.java
@@ -30,13 +30,33 @@ public class StateCensusAnalyser {
     			CSVStateCensus censusData = censusCSVIterator.next();
     		}
     		return numOfEntries;	
-		} catch(IOException e) 
-	    {
+		} catch(IOException e) {
 	    	throw new CensusAnalyserException(e.getMessage(), ExceptionType.CENSUS_FILE_PROBLEM);
-	    }
-	    catch(RuntimeException e) 
-	    {
+	    } catch(RuntimeException e) {
 	    	throw new CensusAnalyserException("CSV File Must Have Comma As Delimiter Or Has Incorrect Header", ExceptionType.CENSUS_WRONG_DELIMITER_OR_HEADER);
 	    }
 	}
+	 public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
+	    	try {
+	        	if(csvFilePath.contains(".txt")) {
+	        		throw new CensusAnalyserException("File must be in CSV Format!", ExceptionType.CENSUS_INCORRECT_FILE_FORMAT);
+	        	}	        	
+	        	Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+	            CsvToBeanBuilder<CSVStates> csvToBeanBuilder = new CsvToBeanBuilder<CSVStates>(reader);
+	            csvToBeanBuilder.withType(CSVStates.class);
+	            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	            CsvToBean<CSVStates> csvToBean = csvToBeanBuilder.build();
+	            Iterator<CSVStates> stateCodesCSVIterator = csvToBean.iterator();	            
+	            int numOfEntries = 0;
+	    		while(stateCodesCSVIterator.hasNext()) {
+	    			numOfEntries++;
+	    			CSVStates censusData = stateCodesCSVIterator.next();
+	    		}
+	    		return numOfEntries;
+	        } catch (IOException e) {
+	            throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+	        } catch(RuntimeException e) {
+	    		throw new CensusAnalyserException("File has wrong delimiter or header", ExceptionType.CENSUS_WRONG_DELIMITER_OR_HEADER);
+	    	}
+	    }
 }
